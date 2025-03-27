d41f09b6a06c929d48803a4e4f2d3e7d6fd3b6e9
egorbelosohov <belosokhov.e@yandex.ru>
2025-03-27 06:25:10 +0300

    added simple test

---
+++ test.simple/test_fail.py
@@ -0,0 +1,22 @@
+
+
+def test_fail1():
+    pass
+
+def test_fail2():
+    pass
+
+def test_fail3():
+    pass
+
+def test_fail4():
+    pass
+
+def test_fail5():
+    pass
+
+def test_fail6():
+    pass
+
+def test_fail7():
+    pass
